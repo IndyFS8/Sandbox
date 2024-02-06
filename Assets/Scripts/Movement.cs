3bfb09a9b813201bbd1efc0d8485828b8e268654
IndyFS8 <indyfs8@gmail.com>
2024-02-06 21:09:22 +0000

    working player and camera

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public GameObject Sphere;
+    public float speed;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,37 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+                {
+
+            Sphere.transform.position += new Vector3(-speed, 0, 0);
+            
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Sphere.transform.position += new Vector3(speed, 0, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Sphere.transform.position += new Vector3(0, 0, -speed);
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Sphere.transform.position += new Vector3(0, 0, speed);
+        }
+
+
+
+
+
+
+
+
+
     }
 }
